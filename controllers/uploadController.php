88337348af65325e9781833abf0be0cb7bc19653
Mahinthan Kengatharan <mahin.than@hotmail.com>
2014-11-21 16:00:28 +0100

    Added controllers

--- controllers/uploadController.php
+++ controllers/uploadController.php
@@ -1,4 +1,28 @@
 <?php
 class uploadController{
 
+    public function __construct(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        switch ($method) {
+            case 'POST':
+                $this->uploadFile();
+                break;
+            default:
+                $this->showForm();
+                break;
+        }
+    }
+
+    //Show default form
+    function showForm(){
+        include('views/create_upload.php');
+        //echo "test";
+    }
+
+    //Post upload form
+    function uploadFile(){
+        echo "uploading";
+    }
+
+
 }
