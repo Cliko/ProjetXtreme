d93616a8e573c8edb78051cfc5877666afd9a918
Christophe Cuadra <cuadra.christophe@gmail.com>
2014-11-21 15:07:23 +0100

    Merge branch 'master' of https://github.com/Cliko/ProjetXtreme
    Conflicts:
    .gitignore

---
+++ views/dl_upload.php
@@ -0,0 +1,4 @@
+<div class="dl_upload">
+    <h3>Nom du fichier</h3>
+    <a href="">Télécharger le fichier</a>
+</div>
---
+++ views/user.php
@@ -0,0 +1,7 @@
+<h1>Modifier votre mot de passe</h1>
+
+<form method="" action="">
+    <label>Votre nouveau mot de passe</label><input type="password" name="new_password" value="" />
+    <label>Réécrire votre nouveau mot de passe</label><input type="password" name="new_password_confirm" value="" />
+    <input type="submit" name="login_valider" value="Valider" />
+</form>
