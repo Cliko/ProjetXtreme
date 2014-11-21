684ba60eef5b92bf025935961f6ea3e006885293
Elodie Barbet <elodie-barbet@cifacom.com>
2014-11-21 14:45:33 +0100

    vues Elodie

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<div class="login">
+    <h2>Log In</h2>
+    <form method="" action="">
+        <input type="text" name="login_pseudo" value="Votre Pseudo" />
+        <input type="password" name="login_password" value="Votre mot de passe" />
+        <input type="submit" name="login_valider" value="Valider" />
+    </form>
+</div>
+
+<div class="signin">
+    <h2>Sign In</h2>
+    <form method="" action="">
+        <input type="text" name="signin_pseudo" value="Votre Pseudo" />
+        <input type="email" name="signin_email" value="Votre Email" />
+        <input type="password" name="signin_password" value="Votre mot de passe" />
+        <input type="submit" name="signin_valider" value="Valider" />
+    </form>
+</div>
