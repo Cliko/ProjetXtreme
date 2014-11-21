9c1b4dfe0423095a2224632736ccb6b4ffd3afde
Florence Clavier <florence.clavier@cifacom.com>
2014-11-21 14:59:43 +0100

    add user page for change password

--- views/user.php
+++ views/user.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Florence
- * Date: 21/11/2014
- * Time: 14:55
- */ 
+<h1>Modifier votre mot de passe</h1>
+
+<form method="" action="">
+    <label>Votre nouveau mot de passe</label><input type="password" name="new_password" value="" />
+    <label>Réécrire votre nouveau mot de passe</label><input type="password" name="new_password_confirm" value="" />
+    <input type="submit" name="login_valider" value="Valider" />
+</form>
