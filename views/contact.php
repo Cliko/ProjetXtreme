8e220aa0520e0968dac4e996a37639ce693fc33b
Elodie Barbet <elodie-barbet@cifacom.com>
2014-11-21 15:15:31 +0100

    vues elo

---
+++ views/contact.php
@@ -0,0 +1,9 @@
+<h2>Contact</h2>
+<p>Afin de répondre, vous pouvez contacter notre équipe technique</p>
+
+<form method="" action="">
+    <label>Email :</label> <input type="email" value="Votre email" name="contact_email" />
+    <label>Objet :</label> <input type="text" value="Votre objet" name="contact_object" />
+    <label>Message :</label> <textarea placeholder="Votre message" name="contact_message"></textarea>
+    <input type="submit" value="Envoyer" name="contact_submit">
+</form>
