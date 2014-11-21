bd0ab703e110d6d8105d725280310d2fe6552e88
Florence Clavier <florence.clavier@cifacom.com>
2014-11-21 15:16:07 +0100

    Télécharger un élément hébergé

---
+++ views/download.php
@@ -0,0 +1,8 @@
+<h1>Télécharger</h1>
+
+<?php /* boucle via la bdd pour récupérer les infos */ ?>
+<ul class="id_<?php echo $id_files; ?>">
+    <li>Fichier : <?php echo "url_fichier"; ?></li>
+    <li>Email envoyé : <?php echo "email_send"; ?></li>
+    <li>Date d'expiration : <?php echo "date_time"; ?></li>
+</ul>
