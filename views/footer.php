22e37eb28136438ae2ec679136b5fe3761028944
Florence Clavier <florence.clavier@cifacom.com>
2014-11-21 15:23:37 +0100

    edit footer + add réseaux sociaux

--- views/footer.php
+++ views/footer.php
@@ -1,1 +1,6 @@
+<footer>
+    <h3>Réseaux sociaux</h3>
+    <p></p><a href="" target="_blank">Facebook</a> - <a href="" target="_blank">Twitter</a> - <a href="" target="_blank">Google+</a></footer>p>
+</footer>
+
 </body>
